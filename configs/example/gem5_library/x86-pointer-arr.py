1e9276d772e1d19ab84d9207fb947e295bedce08
Vadym Musiienko <musiienkovadym@gmail.com>
2026-07-21 18:50:33 -0700

    Update x86-pointer-array to be more like the cxl version

--- configs/example/gem5_library/x86-pointer-arr.py
+++ configs/example/gem5_library/x86-pointer-arr.py
@@ -1,10 +1,10 @@
 """
 This gem5 configuation script creates a simple board to run an X86
-"pointer array microbenchmark" binary.
+"microtest" benchmark binary. (Array of pointers)
 
 This setup is close to the simplest setup possible using the gem5
 library. It does not contain any kind of caching, IO, or any non-essential
-components.
+components. It uses a single local DDR4 memory system.
 
 Usage
 -----
@@ -16,42 +16,57 @@ scons build/ALL/gem5.opt
 """
 
 import argparse
-from socket import (
-    gethostname,
-)  # TODO: Just for the binary path (pcal vs local mac vs hpc)
+import base64
 import os
+from socket import gethostname
 
+import m5
 from gem5.components.boards.simple_board import SimpleBoard
-
+from gem5.components.boards.x86_board import X86Board
 from gem5.components.cachehierarchies.classic.no_cache import NoCache
-from gem5.components.memory import SingleChannelDDR3_1600
+
+# --- Memory imports ---
+from gem5.components.memory import SingleChannelDDR4_2400
 from gem5.components.processors.cpu_types import CPUTypes
 from gem5.components.processors.simple_processor import SimpleProcessor
-from gem5.isas import ISA
-from gem5.simulate.simulator import Simulator
-
-from gem5.components.cachehierarchies.classic.private_l1_cache_hierarchy import (
-    PrivateL1CacheHierarchy,
+from gem5.components.processors.simple_switchable_processor import (  # TODO: processor that supports KVM
+    SimpleSwitchableProcessor,
 )
+from gem5.isas import ISA
 
+# from gem5.resources.resource import BinaryResource, obtain_resource
+from gem5.resources.resource import (
+    BinaryResource,
+    obtain_resource,
+)
+from gem5.simulate.exit_handler import (
+    WorkBeginExitHandler,
+    WorkEndExitHandler,
+)
+from gem5.simulate.simulator import Simulator
+from gem5.utils.override import overrides
 from gem5.utils.requires import requires
-
-# Imports for custom workload
-# from gem5.components.boards.se_binary_workload import SEBinaryWorkload
-from gem5.resources.resource import BinaryResource
 
 # This check ensures the gem5 binary contains the X86 ISA target. If not, an
 # exception will be thrown.
+# TODO: require: , kvm_required = True
 requires(isa_required=ISA.X86)
 
-# --------- Arguments ---------
+# Arguments
+parser = argparse.ArgumentParser(
+    description="Configuration script to run the pointer array microbenchmark on a local DDR4 memory system"
+)
 
-# TODO: add desc
-parser = argparse.ArgumentParser()
+# Full system mode vs SE mode
+parser.add_argument(
+    "-fs", "--full-system", action="store_true", help="Run in full system mode"
+)
 
+# Benchmark specific arguments
 parser.add_argument(
     "--num-threads",
     type=str,
+    default="64",
     required=True,
     # TODO: need better help message
     help="Specify the number of threads 1 - 1000",
@@ -82,37 +97,25 @@ parser.add_argument(
 
 args = parser.parse_args()
 
+FS_MODE = args.full_system
 num_threads = args.num_threads
 array_size = args.array_size
 num_operations = args.num_operations
 random_distr = args.random_distr
-NUM_CORES = 65
 
-# TODO: Add cache similar to local machine
-# TODO: Cache as an argument?
+# Arguments for the binary (pointer array worload)
+arguments = [num_threads, array_size, num_operations, random_distr]
+
+# A single local DDR4 memory system
+memory = SingleChannelDDR4_2400(size="3GiB")
+
+
+# In this setup we don't have a cache. `NoCache` can be used for such setups.
+# TODO: Make cache hierarchy an argument?
 cache_hierarchy = NoCache()
-# cache_hierarchy = PrivateL1CacheHierarchy(l1d_size="4KiB", l1i_size="32KiB")
 
-# TODO: Add memory similar to local machine
-# We use a single channel DDR3_1600 memory system
-memory = SingleChannelDDR3_1600(size="4GiB")
-
-# We use a simple Timing processor with one core.
-# TODO: Choose a cpu similar to local machine
-# TODO: Number of cores has to be >= number of threads (because no OS)
-# TODO: Try either TIMING or O3
-processor = SimpleProcessor(cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=NUM_CORES)
-
-# TODO: Choose a board similar to local machine
-# The gem5 library simple board which can be used to run SE-mode simulations.
-board = SimpleBoard(
-    clk_freq="3GHz",
-    processor=processor,
-    memory=memory,
-    cache_hierarchy=cache_hierarchy,
-)
-
-# TODO: Don't hardcode the workload??
+# Set the workload
+# Actual binary for SE, binary to copy over for FS
 hostname = gethostname()
 if hostname.startswith("Vadym"):  # 'Vadyms-MacBook-Air-2.local'
     WORKLOAD_PATH = "/Users/vadymmusiienko/Work/Research/microtests/bin-intel/microtest"
@@ -123,13 +126,102 @@ elif "SLURM_JOB_ID" in os.environ or hostname == "sagehen.hpc.pomona.edu":
 else:
     raise Exception("Not one of the configured machines! (pcal|hpc|local mac)")
 
-# Set the workload
-binary = BinaryResource(local_path=WORKLOAD_PATH)
-arguments = [num_threads, array_size, num_operations, random_distr]
+# Full system mode setup
+if FS_MODE:
+    # Switchable Processor to run FS mode
+    processor = SimpleSwitchableProcessor(
+        starting_core_type=CPUTypes.ATOMIC,
+        switch_core_type=CPUTypes.TIMING,
+        isa=ISA.X86,
+        num_cores=(int(num_threads) + 1),
+    )
 
-# board.set_se_binary_wordload(workload)
-board.set_se_binary_workload(binary=binary, arguments=arguments)
+    # X86 board to run FS mode
+    board = X86Board(
+        clk_freq="3GHz",
+        processor=processor,
+        memory=memory,
+        cache_hierarchy=cache_hierarchy,
+    )
 
-# Run the simulation
+    # Variable with the entire benchmark binary
+    raw_binary = None
+    with open(WORKLOAD_PATH, "rb") as file:
+        raw_binary = file.read()
+    if not raw_binary:
+        print("Couldn't read the binary file into a variable")
+        exit()
+
+    # Encoding for shell script
+    payload = base64.b64encode(raw_binary).decode("ascii")
+
+    # Commands to run after boot
+    # Copy over the binary (dynamically updates it) and run it
+    # TODO: try without wrap
+    my_commands = (
+        "echo 'Boot complete, updating the binary!'\n"
+        "cat << 'B64EOF' | base64 -d > /root/executable\n"
+        f"{payload}\n"
+        "B64EOF\n"
+        "chmod +x /root/executable\n"
+        f"/root/executable {' '.join(arguments)}\n"
+        "m5 exit\n"
+    )
+
+    board.set_kernel_disk_workload(
+        kernel=obtain_resource(resource_id="x86-linux-kernel-5.4.49"),
+        disk_image=obtain_resource(resource_id="x86-ubuntu-18.04-img"),
+        readfile_contents=my_commands,  # Bash script that runs after boot
+    )
+
+    print("Full-System mode")
+
+    # Custom messages
+    class CustomWorkBeginExitHandler(WorkBeginExitHandler):
+        @overrides(WorkBeginExitHandler)
+        def _process(self, simulator: "Simulator") -> None:
+            print("Done booting Linux")
+            print("Resetting stats at the start of ROI!")
+            m5.stats.reset()
+            simulator.switch_processor()
+
+        @overrides(WorkBeginExitHandler)
+        def _exit_simulation(self) -> bool:
+            return False
+
+    class CustomWorkEndExitHandler(WorkEndExitHandler):
+        @overrides(WorkEndExitHandler)
+        def _process(self, simulator: "Simulator") -> None:
+            print("Dump stats at the end of the ROI!")
+            m5.stats.dump()
+
+        @overrides(WorkEndExitHandler)
+        def _exit_simulation(self) -> bool:
+            return True
+
+else:
+    # SE mode setup
+    processor = SimpleProcessor(
+        cpu_type=CPUTypes.TIMING, isa=ISA.X86, num_cores=(int(num_threads) + 1)
+    )
+
+    board = SimpleBoard(
+        clk_freq="3GHz",
+        processor=processor,
+        memory=memory,
+        cache_hierarchy=cache_hierarchy,
+    )
+
+    binary = BinaryResource(local_path=WORKLOAD_PATH)
+    board.set_se_binary_workload(binary=binary, arguments=arguments)
+
+    print("System-call Emulation mode")
+
+
+# Lastly we run the simulation.
 simulator = Simulator(board=board)
+
+print("Running the simulation")
+# print("Using KVM cpu")
+
 simulator.run()
